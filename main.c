ed4afcc3f6f4738e2c1b3e76aba08adc564e87cc
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-17 18:28:38 +0900

    실습2
    temp = 1
    temp = 2
    temp = 3
    temp = 4
    temp = 5

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 //2. 지역 변수 생존기간 (tmep = 1이 반복 됨) 
+// +1 더해진 채로 나오게 하려면? 
 int main(void)
 {
     int i;
+    int temp = 1;
     
     for (i=0;i<5;i++)
     {
-        int temp = 1;
         printf("temp = %d\n", temp);
-        temp++; 
+        temp++; //+1 증가되는 것이 아님. 
         }
 
   system("PAUSE");	
   return 0;
 }
+//새로 만들었다가 없어졌다가 반복해서 +1이 누적되지 않음.  
