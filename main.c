bf898da9702a5b23b74fe1f5db9380b5eec8a209
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-17 22:02:56 +0900

    실습 6-1

--- main.c
+++ main.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-//5. 저장 유형 지정자 (생존 기간을 늘리고 싶다)
-// static을 붙히면 계속 살아남음
-void sub(void);
+//6-1. 연결 
+int all_files; //전역 변수로 정의 되어있음. extern 
 
-int main(void)
+extern void sub(void); 
+
+int main(int argc, char *argv[])
 {
-    int i; 
-    for (i=0;i<3;i++)
-        sub(); // sub 함수 3번 실행
-         
-    system("PAUSE");	
+    sub();
+    printf("%d\n", all_files);
+    
+    system("PAUSE");
     return 0;
-} 
-
-void sub(void)
-{
-     int auto_count = 0; //자동 할당  
-     static int static_count = 0; //2가지 변수 선언 (auto, static) //정적 할당  
-     
-     auto_count++;
-     static_count++; //값이 누적됨. 
-     
-     printf("auto_count=%d\n", auto_count);
-     printf("static_count=%d\n", static_count);
 }
-//static count는 +1 증가, auto count 는 1로 유지  
---
+++ sub.c
@@ -0,0 +1,5 @@
+extern int all_files; //extern À¸·Î ¿¬°áµÊ.  
+void sub(void)
+{
+     all_files=10;
+} 
