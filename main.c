1e5ebb900af79c410dc0b69d2abe9ec0c52dd7ff
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-17 21:42:54 +0900

    실습 5
    auto_count=1
    static_count=1
    auto_count=1
    static_count=2
    auto_count=1
    static_count=3

--- main.c
+++ main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-//4. 전역 변수
-// # 50개 나오게 해보자  
-void f(void);
+//5. 저장 유형 지정자 (생존 기간을 늘리고 싶다)
+// static을 붙히면 계속 살아남음
+void sub(void);
 
 int main(void)
 {
     int i; 
-    for (i=0;i<5;i++)
-    {
-        f();
-    }
+    for (i=0;i<3;i++)
+        sub(); // sub 함수 3번 실행
+         
     system("PAUSE");	
     return 0;
-} //여기 안에서만 int i; 작용  
-
-void f(void)
-{
-     int i; //그래서 int i 하나 더 필요 
-     for (i=0;i<10;i++)
-     printf("#");
 } 
 
+void sub(void)
+{
+     int auto_count = 0; //자동 할당  
+     static int static_count = 0; //2가지 변수 선언 (auto, static) //정적 할당  
+     
+     auto_count++;
+     static_count++; //값이 누적됨. 
+     
+     printf("auto_count=%d\n", auto_count);
+     printf("static_count=%d\n", static_count);
+}
+//static count는 +1 증가, auto count 는 1로 유지  
